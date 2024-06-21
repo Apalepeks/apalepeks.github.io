4d9086f9ec9372ff2ef799a6048388942b2d0712
Apalepeks <63862536+Apalepeks@users.noreply.github.com>
2024-06-21 05:18:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
+let colors;
+let shapes = [];
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(240);
-    noLoop(); // Draw only once unless mouse interaction occurs
+  createCanvas(windowWidth, windowHeight);
+  colors = [color('#D32F2F'), color('#1976D2'), color('#388E3C'), color('#FBC02D'), color('#8E24AA')];
+  background(255);
 }
 
 function draw() {
-    // Clear background on each draw call to avoid smudging
-    background(240);
-
-    // Kandinsky-inspired geometric shapes
-    for (let i = 0; i < 10; i++) {
-        fill(random(255), random(255), random(255), 150);
-        noStroke();
-        ellipse(random(width), random(height), random(50, 150));
-    }
-
-    // Basquiat-inspired spontaneous lines and shapes
-    for (let i = 0; i < 20; i++) {
-        stroke(random(255), random(255), random(255));
-        strokeWeight(random(1, 5));
-        line(random(width), random(height), random(width), random(height));
-    }
+  for (let shape of shapes) {
+    shape.display();
+  }
 }
 
 function mouseDragged() {
-    // Draw shapes while dragging the mouse
-    fill(random(255), random(255), random(255), 200);
-    noStroke();
-    rect(mouseX, mouseY, random(20, 60), random(20, 60));
-
-    // Add spontaneous lines
-    stroke(random(255), random(255), random(255));
-    strokeWeight(random(1, 5));
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  let shapeType = int(random(3));
+  let col = random(colors);
+  if (shapeType === 0) {
+    shapes.push(new Rectangle(mouseX, mouseY, col));
+  } else if (shapeType === 1) {
+    shapes.push(new Circle(mouseX, mouseY, col));
+  } else {
+    shapes.push(new Triangle(mouseX, mouseY, col));
+  }
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+class Rectangle {
+  constructor(x, y, col) {
+    this.x = x;
+    this.y = y;
+    this.w = random(20, 10);
+    this.h = random(20, 10);
+    this.col = col;
+  }
+  
+  display() {
+    noStroke();
+    fill(this.col);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
+
+class Circle {
+  constructor(x, y, col) {
+    this.x = x;
+    this.y = y;
+    this.r = random(10, 50);
+    this.col = col;
+  }
+  
+  display() {
+    noStroke();
+    fill(this.col);
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
+
+class Triangle {
+  constructor(x, y, col) {
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = x + random(-50, 50);
+    this.y2 = y + random(-50, 50);
+    this.x3 = x + random(-50, 50);
+    this.y3 = y + random(-50, 50);
+    this.col = col;
+  }
+  
+  display() {
+    noStroke();
+    fill(this.col);
+    triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
+  }
 }
